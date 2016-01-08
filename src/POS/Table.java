6c72f12f98f35c9d9d03d114eca63faa820fa585
Suhn Kim <kim.suhnmi@gmail.com>
2016-01-08 10:09:49 -0500

    Table, TableHandler classes added

---
+++ src/POS/Table.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package POS;
+
+
+
+/**
+ *
+ * @author Suhn
+ */
+public class Table {
+    int tableNumber;
+    boolean occupied;
+    long sittingTime;
+    Order order;
+    
+    public Table(){
+        
+    }
+}
---
+++ src/POS/TableHandler.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package POS;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Suhn
+ */
+public class TableHandler {
+    ArrayList<Table> cusTblList;
+    
+    public void addTable(int t){
+        
+    }
+    
+    public void occupyTable(int t){
+        
+    }
+}
